9323e44aec730e4648abe9721b4a18791eda2d6e
nwaila <nc00361553@techmahindra.com>
2017-09-14 09:21:54 +0530

    Introducing My Name

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,10 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world!!!!");
 		System.out.println("World says. Hello too... :)");
+		StringBuilder b = new StringBuilder("neha");
+		System.out.println("My Name Is:"+b.toString());
+		System.out.println("My Name In Reverse: "+b.reverse().toString());
 	}
 
 }
+ 
