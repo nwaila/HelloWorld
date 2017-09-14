a3fbd557a6663b0f702fe4365234e20ad297bed8
nwaila <nc00361553@techmahindra.com>
2017-09-14 14:14:27 +0530

    Commiting my change

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -10,6 +10,10 @@ public class HelloWorld {
 		System.out.println("Great Job, "+b);
 		System.out.println("Thanks :) ");
 		System.out.println("Welcome ");
+<<<<<<< HEAD
+=======
+		System.out.println("Creating new branch HellowWorld1.1");
+>>>>>>> d0b757b... Commiting my change
 	}
 
 }
