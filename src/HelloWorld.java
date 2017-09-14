4206b54ba105f5e9d8e40c7a7a2b2cd80ecc4b22
nwaila <nc00361553@techmahindra.com>
2017-09-14 09:37:45 +0530

    Added surname

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world!!!!");
 		System.out.println("World says. Hello too... :)");
-		StringBuilder b = new StringBuilder("neha");
+		StringBuilder b = new StringBuilder("neha chauhan");
 		System.out.println("My Name Is:"+b.toString());
 		System.out.println("My Name In Reverse: "+b.reverse().toString());
 	}
